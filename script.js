474b49b9d067d66a62757578209c2426a29143b9
LazaroTri10 <55921218+Lazarotri10@users.noreply.github.com>
2024-10-19 23:26:51 -0300

    teste 2

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
     alert("Thank you for your message! We will get back to you soon.");
 });
+
+    // Função para abrir e fechar o menu ao clicar no ícone de hambúrguer
+    const hamburger = document.getElementById('hamburger');
+    const menuLinks = document.getElementById('menuLinks');
+
+    hamburger.addEventListener('click', () => {
+      menuLinks.classList.toggle('active');
+    });
