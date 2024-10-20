a0e668aca4ef392858fd340f13635adb6049ed3a
LazaroTri10 <55921218+Lazarotri10@users.noreply.github.com>
2024-10-19 23:04:30 -0300

    TESTE

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for your message! We will get back to you soon.");
+});
